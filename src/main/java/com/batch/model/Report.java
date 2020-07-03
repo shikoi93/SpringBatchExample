e57a66f6525623a03bc9102906bb2cc9c1985954
shikoi93 <60942853+shikoi93@users.noreply.github.com>
2020-07-02 20:34:57 -0500

    Initial commit

---
+++ src/main/java/com/batch/model/Report.java
@@ -0,0 +1,49 @@
+package com.batch.model;
+
+public class Report {
+	private String Join_Date;
+	private String Id;
+	private String Name;
+	private String Department;
+	private String Age;
+
+	public String getJoin_Date() {
+		return Join_Date;
+	}
+
+	public void setJoin_Date(String join_Date) {
+		Join_Date = join_Date;
+	}
+
+	public String getId() {
+		return Id;
+	}
+
+	public void setId(String id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public String getDepartment() {
+		return Department;
+	}
+
+	public void setDepartment(String department) {
+		Department = department;
+	}
+
+	public String getAge() {
+		return Age;
+	}
+
+	public void setAge(String age) {
+		Age = age;
+	}
+}
